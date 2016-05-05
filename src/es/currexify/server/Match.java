b79b0c115c10e1684d184929057a169aae54f69d
Guillermo Garcia <g.gmenendez@alumnos.upm.es>
2016-05-05 18:29:26 +0200

    Matching v0.5.0 without change rates

--- src/es/currexify/server/Match.java
+++ src/es/currexify/server/Match.java
@@ -30,24 +30,66 @@ public class Match extends HttpServlet {
 		List<TransactionModel> gbp_usd = transdao.readByCurrency(em, "GBP", "USD");
 		List<TransactionModel> usd_gbp = transdao.readByCurrency(em, "USD", "GBP");
 		
-		if(eur_gbp.size() > 0) {
-			if(gbp_eur.size() > 0) {
-				for(TransactionModel trans : eur_gbp) {
-					if(trans.getEDate().after(new Date())){
-						for(TransactionModel trans2 : gbp_eur) {
-							//todo
+		doMatch(eur_gbp, gbp_eur);
+	}
+	
+	private void doMatch (List<TransactionModel> list1, List<TransactionModel> list2) {
+		TransactionModel bestTrans = null; 
+		TransactionModel ndTrans = null;
+		
+		if(list1.size() > 0) {
+			if(list2.size() > 0) {
+				for(TransactionModel trans : list1) {
+					double originAmount = trans.getAmountLeft();
+					double left = originAmount;
+					if(trans.getEDate().after(new Date())){                     	 // SE TIENE QUE HACER AHORA
+						// TODO RIGHT NOW
+						for(TransactionModel trans2 : list2) {
+							double destAmount = trans2.getAmountLeft();
+							if (left == destAmount) {
+								//TODO casarlos
+							} else {
+								if (left > destAmount) {
+									left -= destAmount;
+									// TODO completar trans2 y actualizar trans
+								}
+							}							
 						}
+						// TODO comleta sistema
+					} else { 		           								 // TENEMOS MAZO TIEMPO PARA HACERLO
+						left = originAmount;
+						for (TransactionModel trans2 : list2) {
+							double destAmount = trans2.getAmountLeft();
+							
+							if (originAmount == destAmount) {
+								// TODO casarlos
+							} else if ((originAmount - destAmount) < left) {
+								ndTrans = bestTrans;
+								bestTrans = trans2;
+								left = originAmount - destAmount;
+							} else {
+								continue;
+							}
+						}
+						// TODO casar bestTrans con trans y completar bestTrans
+					}
+				}
+			} else { 																	// LIST2 ESTA TOPE VACIA
+				for (TransactionModel trans : list1) {
+					if (trans.getEDate().after(new Date())) {
+						// TODO completar con el sistema
 					}
 				}
 			}
-			else {
-				//todo
-			}
-		}
-		else {
-			if(gbp_eur.size() > 0) {
-				//todo
+		} else { 																		// LIST1 ESTA TOPE VACIA
+			if (list2.size() > 0) {
+				for (TransactionModel trans : list2) {
+					if (trans.getEDate().after(new Date())) {
+						// TODO completar con el sistema
+					}
+				}
 			}
 		}
 	}
+	
 }
