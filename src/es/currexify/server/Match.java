f3fb8f6258619efcebb394e12b80075d78562c6f
Guillermo Garcia <g.gmenendez@alumnos.upm.es>
2016-05-06 17:55:45 +0200

    Match changed

--- src/es/currexify/server/Match.java
+++ src/es/currexify/server/Match.java
@@ -51,102 +51,64 @@ public class Match extends HttpServlet {
 		Collections.sort(gbp_usd, tmComparator);
 		Collections.sort(usd_gbp, tmComparator);
 		
-		doMatch(eur_gbp, gbp_eur);
+		doBetterMatch(eur_gbp, gbp_eur);
 	}
 	
-	private void doMatch (List<TransactionModel> list1, List<TransactionModel> list2) {
-		TransactionModel bestTrans = null; 
-		TransactionModel ndTrans = null;
+	private void doBetterMatch (List<TransactionModel> list1, List<TransactionModel> list2) {
+		double totalAmount1 = 0;
+		double totalAmount2 = 0;
 		
-		if(list1.size() > 0) {
-			if(list2.size() > 0) {
-				for(TransactionModel trans : list1) {
-					double originAmount = trans.getAmountLeft();
-					double left = originAmount;
-					if(trans.getEDate().after(new Date())){                     	 // SE TIENE QUE HACER AHORA
-						for(TransactionModel trans2 : list2) {
-							double destAmount = trans2.getAmountLeft();
-							double destAmountConverted = getConverted(trans2.getSCoin(), trans2.getDCoin(), destAmount);
-							if (left == destAmountConverted) {
-								//TODO casarlos
-								EntityManager em = EMFService.get().createEntityManager();
-								
-								UsuariosDAOImpl udao = UsuariosDAOImpl.getInstance();
-								UsuariosModel um1 = udao.readUserById(em, trans.getId().getParent().getId());
-								UsuariosModel um2 = udao.readUserById(em, trans2.getId().getParent().getId());
-								
-								HistoryModel hm1s = new HistoryModel(
-										um1.getCardN(), trans.getSCoin(), left, "Saliente", new Date());
-								HistoryModel hm1e = new HistoryModel(
-										um1.getCardN(), trans.getDCoin(), destAmount, "Entrante", new Date());
-								udao.addHistoryToUser(em, hm1s, um1);
-								udao.addHistoryToUser(em, hm1e, um1);
-								
-								HistoryModel hm2s = new HistoryModel(
-										um2.getCardN(), trans2.getSCoin(), destAmount, "Saliente", new Date());
-								HistoryModel hm2e = new HistoryModel(
-										um2.getCardN(), trans2.getDCoin(), left, "Entrante", new Date());
-								udao.addHistoryToUser(em, hm2s, um2);
-								udao.addHistoryToUser(em, hm2e, um2);
-								
-								CurrencyBudgetModel cbm1 = new CurrencyBudgetModel(
-										trans.getCardN(), trans.getDCoin(), destAmount);
-								udao.updateCurrency(em, um1, cbm1);
-								/*List<CurrencyBudgetModel> cbml1 = new ArrayList<CurrencyBudgetModel>();
-								cbml1.add(cbm1);
-								um1.setUserCurrencies(cbml1);
-								*/
-								CurrencyBudgetModel cbm2 = new CurrencyBudgetModel(
-										trans2.getCardN(), trans.getDCoin(), left);
-								udao.updateCurrency(em, um2, cbm2);
-								/*List<CurrencyBudgetModel> cbml2 = new ArrayList<CurrencyBudgetModel>();
-								cbml2.add(cbm2);
-								um1.setUserCurrencies(cbml2);
-								*/
-								em.close();
-								
-							} else {
-								if (left > destAmount) {
-									left -= destAmount;
-									// TODO completar trans2 y actualizar trans
-								}
-							}							
-						}
-						// TODO comleta sistema
-					} else { 		           								 // TENEMOS MAZO TIEMPO PARA HACERLO
-						left = originAmount;
-						for (TransactionModel trans2 : list2) {
-							double destAmount = trans2.getAmountLeft();
-							
-							if (originAmount == destAmount) {
-								// TODO casarlos
-							} else if ((originAmount - destAmount) < left) {
-								ndTrans = bestTrans;
-								bestTrans = trans2;
-								left = originAmount - destAmount;
-							} else {
-								continue;
-							}
-						}
-						// TODO casar bestTrans con trans y completar bestTrans
-					}
-				}
-			} else { 																	// LIST2 ESTA TOPE VACIA
-				for (TransactionModel trans : list1) {
-					if (trans.getEDate().after(new Date())) {
-						// TODO completar con el sistema
-					}
-				}
+		for (TransactionModel tm : list1) {
+//			if (tm.getEDate().getTime() > new Date().getTime()+90000)
+				totalAmount1 += tm.getAmountLeft();
+		}
+		for (TransactionModel tm : list2) {
+//			if (tm.getEDate().getTime() > new Date().getTime()+90000)
+			totalAmount2 += tm.getAmountLeft();
+		}
+		
+		double totalAmount1Conv = getConverted(list1.get(0).getSCoin(), list1.get(0).getDCoin(), totalAmount1);
+		double totalAmount2Conv = getConverted(list2.get(0).getSCoin(), list2.get(0).getDCoin(), totalAmount2);
+		if (totalAmount1 < totalAmount2Conv){
+			for (TransactionModel tm : list1) {
+				// TODO completar todas
 			}
-		} else { 																		// LIST1 ESTA TOPE VACIA
-			if (list2.size() > 0) {
-				for (TransactionModel trans : list2) {
-					if (trans.getEDate().after(new Date())) {
-						// TODO completar con el sistema
-					}
+			for (TransactionModel tm : list2) {
+				if (totalAmount1Conv < tm.getAmountLeft()) {
+					// TODO completar tm hasta donde se pueda
+					
+					totalAmount1Conv = 0;
+					break;
 				}
+				totalAmount1Conv -= tm.getAmountLeft();
+				// TODO completar tm
 			}
 		}
+		else if (totalAmount1 > totalAmount2Conv) {
+			for (TransactionModel tm : list2) {
+				// TODO completar todas
+			}
+			for (TransactionModel tm : list1) {
+				if (totalAmount2Conv < tm.getAmountLeft()) {
+					// TODO completar tm hasta donde se pueda
+					
+					totalAmount2Conv = 0;
+					break;
+				}
+				totalAmount2Conv -= tm.getAmountLeft();
+				// TODO completar tm
+			}
+		}
+		else {
+			for (TransactionModel tm : list1) {
+				// TODO completar todas
+			}
+			for (TransactionModel tm : list2) {
+				// TODO completar todas
+			}
+		}
+		
+		return;	
 	}
 	
 	private String getCurrencySymbol(String currencyName) {
